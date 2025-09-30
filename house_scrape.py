2036358f267b396b2c57792b28b97612edf83dee
yeabwang <yeabsiratesfaye781@gmail.com>
2025-09-30 17:05:09 +0800

    house scraper class

---
+++ house_scrape.py
@@ -0,0 +1,26 @@
+from httpx import Client, Timeout, HTTPError
+from httpx_retry import RetryPolicy, RetryTransport
+from models import HouseScraper
+
+class ScraperService:
+
+    def __init__(self, house_scraper: HouseScraper) -> None:
+        self.time_out = Timeout(connect=30,pool=30,read=30,write=30)
+        self.retry_policy = RetryPolicy(
+            max_retries=house_scraper.max_retries,
+            initial_delay=house_scraper.initial_delay,
+            max_delay=house_scraper.max_delay,
+            multiplier=house_scraper.multiplier,
+            retry_on = lambda x : x>=500
+        )
+        self.url_to_be_scraped = house_scraper.url_tobe_scraped
+    
+    def scrape_url(self) -> str:
+        try:
+            with Client(verify=True, timeout=self.time_out,transport=RetryTransport(policy=self.retry_policy)) as client:
+                response = client.get(url=self.url_to_be_scraped)
+                response.raise_for_status()
+        except HTTPError as e:
+            raise e
+        else:
+            return response.text
